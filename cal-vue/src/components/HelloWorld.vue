7bb2a32886bfcc0436220d3aa274049e04353770
Mr0-0Magician <90989527+Mr0-0Magician@users.noreply.github.com>
2022-05-22 22:59:36 +0430

    adding-options-of-input

--- cal-vue/src/components/HelloWorld.vue
+++ cal-vue/src/components/HelloWorld.vue
@@ -4,9 +4,15 @@
     <table id="cal" dir="ltr">
         <tr>
           <td colspan="5" >
-            
+            <input 
+              style="padding:5px ; text-align: right; "
+              type="int , float"
+              id="output"
+              dir="ltr"
+            >
           </td>
         </tr>
+        
     </table>
 
   </div>
