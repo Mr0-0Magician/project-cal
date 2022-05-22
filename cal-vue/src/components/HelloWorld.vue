628056b624e329937ba2db80e0c5a00cfcd9003c
Mr0-0Magician <90989527+Mr0-0Magician@users.noreply.github.com>
2022-05-22 23:06:14 +0430

    adding-buttons

--- cal-vue/src/components/HelloWorld.vue
+++ cal-vue/src/components/HelloWorld.vue
@@ -12,7 +12,33 @@
             >
           </td>
         </tr>
-        
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
     </table>
 
   </div>
